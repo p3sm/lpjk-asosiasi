272d6da73372c053c8633c98bea50c1c6a3ee6fe
maulanardy <maulanardy.dev@gmail.com>
2020-07-16 22:22:22 +0700

    update

--- app/Http/Controllers/PengajuanNaikStatusController.php
+++ app/Http/Controllers/PengajuanNaikStatusController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\ApiKey;
 use App\User;
+use App\Asosiasi;
 
 class PengajuanNaikStatusController extends Controller
 {
@@ -25,6 +26,13 @@ class PengajuanNaikStatusController extends Controller
      */
     public function ska()
     {
+        $asosiasi = Asosiasi::find(Auth::user()->asosiasi->asosiasi_id);
+        $verifikatorSigns = $asosiasi->verifikatorSign->where("provinsi_id", Auth::user()->asosiasi->provinsi_id);
+        $databaseSigns = $asosiasi->databaseSign->where("provinsi_id", Auth::user()->asosiasi->provinsi_id);
+        $verifikatorSign = $verifikatorSigns[array_rand($verifikatorSigns->toArray())]->path;
+
+        dd($verifikatorSigns);
+
         return view('pengajuan_status/indexSKA');
     }
     public function skt()
